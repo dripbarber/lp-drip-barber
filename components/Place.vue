f4e53b513bdb9d9d4da2d9246266a2650b7ae301
Isaac Vieira <isaacvf.dev@gmail.com>
2023-11-14 14:43:10 -0300

    Changed place img

--- components/Place.vue
+++ components/Place.vue
@@ -1,11 +1,11 @@
 <template>
   <section
     id="place_section"
-    class="w-full  md:min-h-screen z-[5] h-[50vh] relative overflow-hidden"
+    class="w-full  md:min-h-screen z-[5] relative overflow-hidden"
   >
     <NuxtImg
       format="webp"
-      src="/assets/place-01.jpeg"
+      src="/assets/place.jpg"
       alt="place background"      
       loading="lazy"
       class="w-full h-full object-cover object-center"
