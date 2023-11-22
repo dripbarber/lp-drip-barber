3f7e62a3c71e041e620c564f52d9e0e271b0ce89
Isaac Vieira <isaacvf.dev@gmail.com>
2023-11-22 15:49:44 -0300

    Changes in gallery imgs

--- components/Gallery.vue
+++ components/Gallery.vue
@@ -3,83 +3,51 @@
     id="gallery_section"
     class="w-full pb-8 md:pb-12 lg:pb-16 pt-10 md:pt-16 lg:pt-24 md:px-4 h-full relative overflow-hidden md:flex md:items-center md:justify-center"
     >
-    <!-- <div class="z-[5] w-full md:flex hidden max-w-6xl mx-auto">
-      <ul>
-        <li class="lg:basis-48 md:basis-36 basis-24 ">
-          <img 
-            src="assets/gallery-item-1.png"
-            alt="imagem da galeria de fotos"
-            class=" object-cover h-auto w-full mx-auto align-middle rounded"  
-          />
-        </li>
-      </ul>
-    </div> -->
-    <div class="z-[5] w-full md:flex hidden max-w-6xl mx-auto">
-      <div class="mr-4">
-        <NuxtImg
-          format="webp"
-          src="assets/gallery-item-1.png"
-          class="w-full aspect-square"
-          alt="imagem da galeria de fotos"
-          fit="inside"
-          loading="lazy"
-          width="540"
-          height="574"
-        />
-        <div class="flex pt-6 overflow-hidden">
-          <NuxtImg
-            format="webp"
-            src="assets/gallery-item-2.png"
-            class="w-1/2 aspect-w-1 mr-auto"
-            alt="imagem da galeria de fotos"                  
-            loading="lazy"
-            width="255"
-            height="274"
-          />
-          <NuxtImg
-            format="webp"
-            src="assets/gallery-item-3.png"
-            class="w-1/2 aspect-w-1  ml-4"
-            alt="imagem da galeria de fotos"
-            loading="lazy"
-            width="255"
-            height="274"
-          />
+    <div class="container hidden md:flex mx-auto px-5 lg:px-44">
+      <div class="-m-1 flex flex-wrap md:-m-2">
+        <div class="flex w-1/2 flex-wrap">
+          <div class="w-1/2 p-1 md:p-2">
+            <img
+              alt="gallery"
+              class="block h-full w-full rounded-lg object-cover object-center"
+              src="assets/gallery-item-2.png" />
+          </div>
+          <div class="w-1/2 p-1 md:p-2">
+            <img
+              alt="gallery"
+              class="block h-full w-full rounded-lg object-cover object-center"
+              src="assets/gallery-item-3.png" />
+          </div>
+          <div class="w-full p-1 md:p-2">
+            <img
+              alt="gallery"
+              class="block h-full w-full rounded-lg object-cover object-center"
+              src="assets/gallery-item-1.png" />
+          </div>
+        </div>
+        <div class="flex w-1/2 flex-wrap">
+          <div class="w-full p-1 md:p-2">
+            <img
+              alt="gallery"
+              class="block h-full w-full rounded-lg object-cover object-center"
+              src="assets/gallery-item-6.png" />
+          </div>
+          <div class="w-1/2 p-1 md:p-2">
+            <img
+              alt="gallery"
+              class="block h-full w-full rounded-lg object-cover object-center"
+              src="assets/gallery-item-5.png" />
+          </div>
+          <div class="w-1/2 p-1 md:p-2">
+            <img
+              alt="gallery"
+              class="block h-full w-full rounded-lg object-cover object-center"
+              src="assets/gallery-item-4.png" />
+          </div>
         </div>
       </div>
-
-      <div class="ml-4">
-        <div class="flex pb-6 overflow-hidden">
-          <NuxtImg
-            format="webp"
-            src="assets/gallery-item-4.png"
-            class="w-1/2 aspect-w-1 mr-auto"
-            alt="imagem da galeria de fotos"
-            loading="lazy"
-            width="255"
-            height="274"
-          />
-          <NuxtImg
-            format="webp"
-            src="assets/gallery-item-5.png"
-            class="w-1/2 aspect-w-1 ml-4"
-            alt="imagem da galeria de fotos"
-            loading="lazy"
-            width="255"
-            height="274"
-          />
-        </div>
-        <NuxtImg
-          format="webp"
-          src="assets/gallery-item-6.png"
-          class="w-full aspect-square object-bottom"
-          alt="imagem da galeria de fotos"
-          loading="lazy"
-          width="540"
-          height="574"
-        />
-      </div>
-    </div>
+    </div>   
+   
     <!--Mobile slides-->
     <div class="md:hidden px-6 sm:px-20">
       <Swiper
